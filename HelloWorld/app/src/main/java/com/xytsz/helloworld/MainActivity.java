9ff914d2b8ddaeb64786acc7a61002250f38949a
zhaobingbing <hjbing456@126.com>
2017-05-10 15:19:24 +0800

    第二次推送

--- HelloWorld/app/src/main/java/com/xytsz/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/xytsz/helloworld/MainActivity.java
@@ -1,13 +1,34 @@
 package com.xytsz.helloworld;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv)
+    TextView tv;
+    @BindView(R.id.bt)
+    Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+    }
+
+    @OnClick(R.id.bt)
+    public void onViewClicked() {
+        Intent intent = new Intent(this,NewActivity.class);
+        startActivity(intent);
     }
 }
